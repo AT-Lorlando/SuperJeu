ea8cfacb73002cecb94ca70fcee986d06b6c1f10
Lucas Lévesque <lucas.levesque@insa-cvl.fr>
2020-10-13 14:35:16 +0200

    ~Hexagons (+sub +mul +rmul)

--- Hexagons/Hex.py
+++ Hexagons/Hex.py
@@ -5,7 +5,7 @@ class Hex:
         self.q=q
         self.r=r
         self.s= (s if s!=None else int(-q-r))
-        assert self.s+self.q+self.r==0 , "La somme des trois coordonnées doit valoir 0 !"
+        assert self.s+self.q+self.r==0 , "The sum of the 3 coordinates should be equal to 0!"#condition so that all the hexagons are in the same plan
 
     def __eq__(self, other) :
         return self.q==other.q and self.r==other.r and self.s==other.s
@@ -13,8 +13,20 @@ class Hex:
     def __add__(self, other) :
         return Hex(self.q+other.q,self.r+other.r,self.s+other.s)
 
+    def __sub__(self, other) :
+        return Hex(self.q-other.q,self.r-other.r,self.s-other.s)
+
+    def __mul__(self, x) :
+        return Hex(self.q*x,self.r*x,self.s*x)
+    
+    def __rmul__(self, x) :
+        return self * x
+
     def neighbor(self, direction) :
         return self+axial_hex_directions[direction]
+    
+    def neighbors(self) :
+        return [self.neighbor(i) for i in range(6)]
 
 axial_hex_directions = [Hex(1,0),Hex(0,1),Hex(-1,1),Hex(-1,0),Hex(0,1),Hex(1,-1)] #clockwise orientation, starting from the hexagon on the right
 
@@ -23,10 +35,6 @@ axial_hex_directions = [Hex(1,0),Hex(0,1),Hex(-1,1),Hex(-1,0),Hex(0,1),Hex(1,-1)
 
 
 a=Hex(1,2)
-b=Hex(1,2,-3)
 print(f"{a.q} {a.r} {a.s}")
-print(a==b)
-print(a!=b)
-
-a.neighbor(0)
+a=2*a
 print(f"{a.q} {a.r} {a.s}")
