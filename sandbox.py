5485bf611a815e4e95f3c27350915749ae571ff4
Clement <clement.mellier@insa-cvl.fr>
2020-11-27 23:35:17 +0100

    test json

--- sandbox.py
+++ sandbox.py
@@ -1,40 +1,22 @@
-from os import path
-import pygame
+import json as json
 
+dictionary=dict()
+for i in range(0,ord('z')+1-ord('a')):
+    dictionary[i]=chr(ord('a')+i)
 
-pygame.init()
+# print(dictionary)
 
-run =True
-FPS = 60
-WIDTH, HEIGTH  = 1097, 720
-clock = pygame.time.Clock()
+with open("test.json", mode = 'w') as f :
+    json.dump(dictionary, f)
+    print(dictionary)
 
-main_font = pygame.font.SysFont("Blue Eyes.otf",50)
-screen = pygame.display.set_mode((WIDTH,HEIGTH))
-pygame.display.set_caption("First screen")
-img_dir=path.join(path.dirname(__file__),'Jeu exemple' ,'imgs')
-sprite_dir=path.join(img_dir , 'img_sprite')
+with open("test.json", mode = 'r') as f :
+    dict=json.load(f)['2']
+    print(dict)
 
-bg=pygame.image.load(path.join(img_dir,"map.png")).convert_alpha()
+# get = dict.get('2')
 
-run = True
-while run :
-    screen.blit(bg,(0,0))
-
-    rect = screen.get_rect()
-    f = pygame.font.Font(None, 20)
-    # screen.fill((10, 10, 10))
-    font_surface = main_font.render("   bar", True,(255, 255, 255))
-    font_rect = font_surface.get_rect()
-    print(font_rect)
-    font_rect.topleft = rect.topleft
-    screen.blit(font_surface, font_rect, font_rect)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run  = False
-    
-    pygame.display.update()
-
+# with open("test.json",mode = 'w') as f :
+#     json.dump(get,f)
 
 
