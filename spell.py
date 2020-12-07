4bf471b8a3473c47087eded2f4e3b92ad569c26e
Placarddufutur <71332023+Placarddufutur@users.noreply.github.com>
2020-12-07 18:20:23 +0100

    +comment

--- spell.py
+++ spell.py
@@ -1,11 +1,12 @@
 class Spell ():
     def __init__(self,zone,key,cooldown):
         self.zone = zone # zone is a table of the affected hex in relatif point of view (ex : (0,0) if the spell affects only one case) 
-        self.key = key
-        self.cooldown = cooldown 
-        self.countdown
+        self.key = key #the key on the keyboard
+        self.cooldown = cooldown # base stat of the spells
+        self.countdown # remaining rounds
     
-    def hexTouched(self,pos_x,pos_y):
+    def hexTouched(self,pos_x,pos_y,x_map,y_map):
+        
         return [(pos_x+self.zone.x,pos_y+self.zone.y) for each_case in self.zone]
 
     def isPressed(self,key_pressed):
@@ -16,7 +17,7 @@ class Spell ():
         self.countdown = self.cooldown
     
     def turnPassed(self):
-        self.countdown -=1
+        self.countdown -=1 #need to make a loop to apply this method to all the spell
 
 
 
