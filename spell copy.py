cee97ed51b37e4d8897b7ca2aad1ec7e0b186a30
Lucas Lévesque <lcs.levesque@gmail.com>
2020-12-30 20:21:44 +0100

    ~détail spell

--- spell copy.py
+++ spell copy.py
@@ -1,10 +1,33 @@
 import Hex as hx
 class Spell :
     owner = None
-    castzone = [hx.Hex()]
+    championClasse = None
+    castzone = [hx.Hex()] #relative
     dammagezone = [hx.Hex()]  #relative
-    def dammage_compute(self,Grid, position):
+    surtuile = None
+
+    def cast(self,Grid, position):
         hex_touched = []
         for hexes in self.dammagezone:
-            tile_touched.append(position+hexes)
+            hex_touched.append(position+hexes)
         for hexes in hex_touched :
+            #To-do : hex to tiles
+            tuiles_touches = []
+            pass
+        for tuiles in tuiles_touches :
+            self.surtuile(self.owner,tuiles)
+        return
+
+
+    def __init__(self, fc) :
+        self.surtuile = fc
+        self.surtuile("","")
+
+
+def boule_de_feu(owner, tuile) :
+    # dmg = owner.int * 5
+    # tuile.object.hp -= dmg
+    print("cc")
+    return 
+
+bdf = Spell(boule_de_feu)
