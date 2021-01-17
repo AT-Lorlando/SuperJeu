4acadc37c5685dc92a327e0407374207a138107e
Jérémy <rcd.jeremy@gmail.com>
2021-01-17 20:21:54 +0100

    Revert "Add explosion sound"
    This reverts commit 21c9744aea0099d6b513d4d4f0db88d8e3ca4720.

--- spell.py
+++ spell.py
@@ -1,6 +1,5 @@
 import pygame as pg
 from settings import *
-from sound import *
 vec = pg.math.Vector2
 
 
@@ -72,7 +71,6 @@ class Spell (pg.sprite.Sprite):
 
     def explode(self):
         self.game.animation_add(self.explodes_images, self, colorkey=(223,222,223))
-        explosionSound.play()
 
     def update(self):
         now = pg.time.get_ticks()
